247a57953578ac16c97b68d0cc1d95ce3274744e
Огурцы <kekov312@gmail.com>
2026-05-26 06:25:24 +0000

    add deleta tracks

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const formEl = document.querySelector("form.form")
 const inputEl = document.querySelector(".form__input")
 const habitContainerEl = document.querySelector(".habit-list")
+const DAYS = ['Пн','Вт','Ср','Чт','Пт','Сб','Вс']
 
 // let habits = []
 
@@ -14,14 +15,28 @@ function saveHabits(habits) {
     localStorage.setItem('habits', JSON.stringify(habits))
 }
 
+function createDays (habit) {
+    return DAYS.map((day, i) => `
+        <div data-id=${habit.id} data-index = ${i} class = "day${habit.done.includes(i) ? " day--done" : ""}>
+            <span class="day__label">${day}</span>
+            <div class="day__circle"></div>
+        </div>
+        `,
+    ).join('')
+
+
+
+}
+
+
 function renderHabit(habit) {
     return `
     <li class="habit">
           <div class="habit__header">
           <h2 class="habit__name">${habit.name}</h2>
-          <button class="habit__delete">&cross;</button>
+          <button data-id=${habit.id} class="habit__delete">&cross;</button>
       </div>
-      <div class="habit__days"></div>
+      <div class="habit__days">${createDays(habit)}</div>
     </li>
 `
 }
@@ -41,7 +56,8 @@ function handleSubmit(e) {
     const newHabit = {
 
         name: inputEl.value.trim(),
-        id: Date.now()
+        id: Date.now(),
+        done:[0,1,2,3,4,5,6],
 
     }
 
@@ -59,6 +75,47 @@ function handleSubmit(e) {
     inputEl.value = ""
 }
 
+function handleHabit(e) {
+    const delHabitBtn = e.target.closest('.habit__delete')
+
+    if (delHabitBtn) {
+        const habitId = delHabitBtn.dataset.id
+
+        const oldHabits = loadHabits()
+        
+        const newHabits = oldHabits.filter(el => el.id != habitId)
+        saveHabits(newHabits)
+        render()
+        console.log(index,id);
+        
+    }
+
+    const dayHabitBtn = e.target.closest (".day")
+
+    if (dayHabitBtn) {
+        const id = dayHabitBtn.dataset.it
+        const index = dayHabitBtn.dataset.index
+        const habits = loadHabits()
+
+        const newHabit = habits.map( el => {
+            if(el.id !== (id)) return el
+
+            const isDone = el.done.includes(index)
+
+            const a = {...el,
+                done: isDone
+                    ? el.done.filter((day) => day !== index)
+                    : [... el.done, index],
+            }
+        console.log(a)
+        return a
+        })
+        
+
+    }
+}
+
+habitContainerEl.addEventListener("click", handleHabit)
 formEl.addEventListener ('submit', handleSubmit)
 
 render()
